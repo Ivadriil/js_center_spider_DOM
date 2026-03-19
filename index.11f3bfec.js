5da06efbb633d04263c3aac6a6f74f2f1c50a5b5
Ivadriil <petrivi51@gmail.com>
2026-03-19 21:27:17 +0200

    make build

---
+++ index.11f3bfec.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=(t.clientWidth-e.offsetWidth)/2,l=(t.clientHeight-e.offsetHeight)/2;e.style.top="".concat(l,"px"),e.style.left="".concat(c,"px");
+//# sourceMappingURL=index.11f3bfec.js.map
